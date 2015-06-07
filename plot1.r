0210908ca2e8283fd07dd18f8eb6eee435f95fbd
Tanveer <khantanveera@gmail.com>
2015-06-07 20:45:26 +0530

    excercise exploratory data analysis #2

--- plot1.r
+++ plot1.r
@@ -3,4 +3,9 @@ if ( !"load.R" %in% list.files()
 setwd("C:\\Data Science\\ExData_Plotting1\\ExData_Plotting1")
 }
 source("load.R")
+png(filename = "plot.png",
+    width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white")
+
 with(data_feb,hist(Global_active_power,col="red",ylim=c(0,1200),main="Global Active Power",xlab="Global Active Power (kilowatts)"))
+dev.off()
