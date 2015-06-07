c324c3cc77306a61973f196d140d690198f71746
Tanveer <khantanveera@gmail.com>
2015-06-08 01:26:54 +0530

    excercise exploratory data analysis #2

--- plot4.r
+++ plot4.r
@@ -1,8 +1,13 @@
-if ( ! "load.R" %in% list.files())
-{
-setwd("C:\\Data Science\\ExData_Plotting1\\ExData_Plotting1")
-}
-source("load.R")
+input <- read.table("C:\\stat_learning\\household_power_consumption.txt",header=T,comment.char = "",sep=";",na.string="?",nrow=5)
+classes <- sapply(input,class)
+input <- read.table("C:\\stat_learning\\household_power_consumption.txt",header=T,comment.char = "",sep=";",na.string="?",colClasses=classes)
+attach(input)
+names(input)
+dim(input)
+x <- paste(Date, Time)
+input$DateTime <- strptime(x,"%d/%m/%Y	%T")
+data_feb <- subset(input,Date == "1/2/2007" | Date == "2/2/2007")
+
 png(filename = "plot4.png",
     width = 480, height = 480)
 
